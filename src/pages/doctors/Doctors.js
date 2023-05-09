afe2a58d4eade6c89beab8ac971122eeb3eebd0a
pheobeayo <47613365+pheobeayo@users.noreply.github.com>
2023-05-09 13:37:34 +0100

    admin-dashboard
    This is the admin dashboard

---
+++ src/pages/doctors/Doctors.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { DoctorsParent } from "./doctors.styles";
+
+
+
+const Doctors =() => {
+return (
+    <DoctorsParent></DoctorsParent>
+)
+
+
+}
+
+export default Doctors;
